bc3abe40c66db757f4180aad7ddc300702402e59
Nian <i19850511@gmail.com>
2020-09-14 18:18:08 +0800

    fix:add1

---
+++ helper/utils.go
@@ -0,0 +1,43 @@
+/*
+ * @Date: 2018-07-17 19:37:32
+ * @LastEditors: liunian
+ * @LastEditTime: 2020-09-14 18:17:43
+ */
+package helper
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Response struct {
+	Code int         `json:"code"`
+	Msg  string      `json:"msg"`
+	Data interface{} `json:"data"`
+}
+
+func ResponseWithJson(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+package helper
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Response struct {
+	Code int         `json:"code"`
+	Msg  string      `json:"msg"`
+	Data interface{} `json:"data"`
+}
+
+func ResponseWithJson(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
